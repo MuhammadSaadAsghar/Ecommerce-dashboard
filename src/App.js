d76dc3d77788f52ef6730942f57fef88fc938069
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-02-16 23:12:45 +0500

    create sidebar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Header from './components/Header/Header';
+import Sidebar from './components/sideBar1/Sidebar';
 
 function App() {
   return (
 
 <BrowserRouter>
 <Header/>
+<div className="main d-flex">
+  <div className="sidebar-wrapper">
+  <Sidebar/>
+</div>
+
+<div className="content">
 <Routes>
   <Route path={'/'} exact={true} element={<Dashboard/>} />
+  <Route path={'/Dashboard'} exact={true} element={<Dashboard/>} />
 </Routes>
+</div>
+</div>
 </BrowserRouter>
   );
 }
---
+++ src/components/sideBar1/Sidebar.js
@@ -0,0 +1,186 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import "./Sidebar.css";
+import { FaAngleRight } from "react-icons/fa6";
+import { MdDashboard } from "react-icons/md";
+import { FaProductHunt } from "react-icons/fa6";
+import { IoCart } from "react-icons/io5";
+import { FaBell } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import { IoSettings } from "react-icons/io5";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <MdDashboard />{" "}
+            </span>
+            Dashboard
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <FaProductHunt/>{" "}
+            </span>
+            Products
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoCart/>{" "}
+            </span>
+            Orders
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <MdMessage/>{" "}
+            </span>
+            Messages
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <FaBell/>{" "}
+            </span>
+            Notifications
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100 ">
+            <span className="icon">
+              <IoSettings/>{" "}
+            </span>
+            Setting
+            <span className="arrow ">
+              <FaAngleRight />
+            </span>
+          </Button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
